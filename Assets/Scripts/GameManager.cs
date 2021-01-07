747cf1612bd76204969d0bdcf45ff40ab2a23e08
Jason Hyland <jason.hyland@gmail.com>
2021-01-07 19:17:53 +0000

    Added test and UI stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRateSecs = 1f;
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnRateSecs);
+
+            Instantiate(targets[Random.Range(0,targets.Count)]);
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+
+        body.AddForce(GetRandomForce(), ForceMode.Impulse);
+        body.AddTorque(GetRandomTorque(), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4, 4), -2);
+    }
+
+    Vector3 GetRandomForce()
+    {
+        return Vector3.up * Random.Range(12, 20);
+    }
+
+    Vector3 GetRandomTorque()
+    {
+        return new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
